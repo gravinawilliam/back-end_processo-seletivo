a13db826c188e9215feed2ea13d00e15751e3bc7
gravinawilliam <williamgravina.programador@gmail.com>
2021-02-04 07:00:38 -0300

    refactor: recofigurei o eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,45 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+    env: {
+      es2021: true,
+      node: true,
+      jest: true
     },
-    "extends": [
-        "airbnb-base"
+    extends: [
+      'airbnb-base',
+      'plugin:@typescript-eslint/recommended',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/camelcase': 'off',
+      'no-useless-constructor': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '_',
+        },
+      ],
+      'class-methods-use-this': 'off',
+      camelcase: 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+  };
+  
